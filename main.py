6e32e06619a1d16adebe0b29936335acebe67007
Gang9292 <tracelover38@gmail.com>
2024-05-08 16:15:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from pyrogram import client,filterS
+API_ID =  "704521710"
+API_HASH = "5784145344:AAH9CoBLyVpGAqMHghzpXd_kQove3vD9oRk"
+BOT_TOKEN = "5784145344:AAH9CoBLyVpGAqMHghzpXd_kQove3vD9oRk"
 
+RAINC + Client(
+  name="pyrogramBot",
+  api_id=API_ID,
+    api_hash=API_HASH,
+  bot_token=BOT_TOKEN
+)
+PRINT("Bot Started")
+
+RAINC.run()
